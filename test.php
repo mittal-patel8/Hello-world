4bfea08a3e9e9c11ce7732c91ca26074b891a908
mittal-patel <mittal.hikebranding@gmail.com>
2019-03-12 12:50:44 +0530

    msg

--- test.php
+++ test.php
@@ -1,6 +1,15 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
 echo="test";
 echo="how are you";
 echo="I am fine";
 echo="let's meet up";
 ?>
+
+</body>
+</html>
